bc46531e46085f1b64136c86f07e1fe582aefc14
Fahid21 <mdfahidhasn871@gmail.com>
2023-05-03 19:13:23 +0600

    banner section done

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,19 +7,37 @@ const Home = () => {
       useEffect(() => {
         fetch("http://localhost:5000/meals")
           .then((res) => res.json())
-          .then((data) => console.log(data))
+          .then((data) => setMeals(data))
           .catch((error) => console.log(error));
       }, []);
     return (
-      <div>
+      <div className="md:container sm:mx-auto md:ml-24">
         <p>homeeee</p>
-        
-        <div className="grid grid-cols-3 gap-4 mx-auto">
-            <div className=" bg-slate-500 ...">
-             <LeftNav></LeftNav>
+        <div className="grid gap-6 mb-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 rounded-lg">
+          {meals.map((meal) => (
+            <div className="overflow-hidden relative transition duration-200 transform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl">
+              <img
+                className="object-fill w-full h-56 md:h-56 xl:h-80"
+                src={meal.strMealThumb}
+                alt=""
+              />
+              <div className="bg-amber-700 px-6 py-4 bg-opacity-75 opacity-0 hover:opacity-100 text-gray-300 absolute inset-0 transition-opacity duration-200 pt-32 text-center ">
+                <p className="font-serif text-2xl ">{meal.strMeal}</p>
+
+                <p className="mt-auto">Price: {meal.price}TK</p>
+              </div>
             </div>
-            <div className=" bg-green-100 lg:col-span-2 "><RightNav></RightNav></div>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-3 gap-4 mx-auto">
+          <div className=" bg-slate-500 ...">
+            <LeftNav></LeftNav>
           </div>
+          <div className=" bg-green-100 lg:col-span-2 ">
+            <RightNav></RightNav>
+          </div>
+        </div>
       </div>
     );
 };
--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -5,10 +5,10 @@ import Footer from '../Components/Footer';
 import LefNav from '../Components/LefNav';
 import RightNav from '../Components/RightNav';
 import Home from '../Components/Home';
-
+import bg from '../assets/bg.jpg'
 const Main = () => {
     return (
-      <div className="bg-red-100">
+      <div className="bg-red-100" style={{backgroundImage:`url(${bg})`,backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
         <Header></Header>
         <div className="min-h-[calc(100vh-136px)] ">
           <Outlet></Outlet>
